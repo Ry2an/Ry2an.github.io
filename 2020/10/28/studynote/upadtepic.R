66260a774cfe9f2bf97fdb465dc8c65778f3e228
Ruoyan <ruoyan.han94@outlook.com>
2020-11-21 18:25:55 +0800

    Site updated: 2020-11-21 18:25:13

--- 2020/10/28/studynote/upadtepic.R
+++ 2020/10/28/studynote/upadtepic.R
@@ -17,3 +17,21 @@ for(i in 0:44){
     )
   }
 }
+
+# for(i in 30:47){
+#   if(i < 10){
+#     cat(
+#       "![](meta-analysis-learning/Meta分析的统计方法教程_0", 
+#       i,
+#       ".png)\n\n",
+#       sep = ""
+#     )
+#   }else{
+#     cat(
+#       "![](meta-analysis-learning/Meta分析的统计方法教程_", 
+#       i,
+#       ".png)\n\n",
+#       sep = ""
+#     )
+#   }
+# }
